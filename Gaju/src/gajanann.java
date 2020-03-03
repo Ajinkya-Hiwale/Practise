c1faf59ba2cbade9e35f615e299b6f7f05c8595b
Ajinkya <Ajinkya@Ajinkya-PC>
2020-03-03 22:43:20 +0530

    first commit

---
+++ Gaju/src/gajanann.java
@@ -0,0 +1,11 @@
+
+public class gajanann {
+	public static void main(String[] args)
+	{ 
+		boolean a=false;
+		System.out.println(a);
+		System.out.println("gajanan");
+	}
+	
+
+}
---
+++ Gaju/src/gaju.java
@@ -0,0 +1,11 @@
+
+public class gaju {
+public void main(String args[])
+{
+	
+System.out.println("hello gajanan");
+
+
+
+}
+}
